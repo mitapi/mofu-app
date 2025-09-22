1a1b099be56545c11bf49d72a89b4b63bf348bbc
mitapi <popoyo495@gmail.com>
2025-09-22 21:50:01 +0900

    controllerとrouteè¨作成

---
+++ app/controllers/plays_controller.rb
@@ -0,0 +1,3 @@
+class PlaysController < ApplicationController
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
+  resource :play, only: %i[show] 
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
